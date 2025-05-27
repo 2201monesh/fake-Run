693ed45113656eeb7105b32e7bfeaacd4cf7cdae
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-05-27 20:59:16 +0530

    Global state declarations

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,9 +4,38 @@ const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [totalDistance, setTotalDistance] = useState(false);
+  const [paceUnit, setPaceUnit] = useState();
+  const [duration, setDuration] = useState();
+  const [elevation, setElevation] = useState();
+  const [speed, setSpeed] = useState();
+  const [pace, setPace] = useState();
+  const [runName, setRunName] = useState();
+  const [date, setDate] = useState();
+  const [description, setDescription] = useState();
 
   return (
-    <AppContext.Provider value={{ totalDistance, setTotalDistance }}>
+    <AppContext.Provider
+      value={{
+        totalDistance,
+        setTotalDistance,
+        paceUnit,
+        setPaceUnit,
+        duration,
+        setDuration,
+        elevation,
+        setElevation,
+        speed,
+        setSpeed,
+        pace,
+        setPace,
+        runName,
+        setRunName,
+        date,
+        setDate,
+        description,
+        setDescription,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
