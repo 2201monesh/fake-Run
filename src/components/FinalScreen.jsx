6bae16624c46952f1b27acc76f60601c07db7751
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-05-29 20:59:40 +0530

    Added map view on final screen

--- src/components/FinalScreen.jsx
+++ src/components/FinalScreen.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { useAppContext } from "../context/AppContext";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function FinalScreen() {
-  const { totalDistance, duration, stepsTaken, speed, unit } = useAppContext();
+  const {
+    totalDistance,
+    duration,
+    stepsTaken,
+    speed,
+    unit,
+    coordinates,
+    pathCoords,
+  } = useAppContext();
 
   const cards = [
     { title: "Distance", value: `${totalDistance} km` },
@@ -11,10 +21,29 @@ function FinalScreen() {
     { title: "Speed", value: `${speed} ${unit}` },
   ];
 
+  const center = pathCoords.length > 0 ? pathCoords[0] : [28.6129, 77.2295];
+
   return (
     <div className="w-[100%] h-[100%] flex flex-col justify-center items-center border">
-      <div className="w-[65%] h-[75%] border mt-6">
-        <p className="text-xl font-semibold">final screen</p>
+      <div className="w-[65%] h-[75%] border overflow-hidden mt-6">
+        <MapContainer
+          center={coordinates}
+          zoom={13}
+          scrollWheelZoom={true}
+          dragging={true}
+          doubleClickZoom={true}
+          zoomControl={true}
+          attributionControl={false}
+          style={{ width: "100%", height: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution=""
+          />
+          {pathCoords.length > 1 && (
+            <Polyline positions={pathCoords} color="blue" weight={4} />
+          )}
+        </MapContainer>
       </div>
 
       {/* Cards container */}
